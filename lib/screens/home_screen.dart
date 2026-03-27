15cc6764c1a02f0be399a3f24d383f1178396f81
Lukasz <intergenixyt@gmail.com>
2026-03-27 22:52:47 +0100

    not fully

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,28 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:portfolio6/utils/_utils.dart';
 import 'package:portfolio6/widgets/gen_sliver_app_bar.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late final ScrollController _scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('build');
+
     return Scaffold(
       body: CustomScrollView(
         slivers: [
           GenSliverAppBar(),
 
-          Container(
-            width: 50,
-            height: 300,
-            color: Colors.redAccent,
-          ).sliver,
+          ...[
+            SliverGap(10),
 
-          Container(
-            width: 50,
-            height: 300,
-            color: Colors.amber,
-          ).sliver,
+            Container(
+              width: 50,
+              height: 30,
+              color: Colors.black,
+            ).sliver,
+
+            Container(
+              width: 50,
+              height: 300,
+              color: Colors.redAccent,
+            ).sliver,
+
+            Container(
+              width: 50,
+              height: 300,
+              color: Colors.amber,
+            ).sliver,
+          ].withHorizontalPadding(32).withSafeArea(),
         ],
       ),
     );
--- lib/widgets/gen_sliver_app_bar.dart
+++ lib/widgets/gen_sliver_app_bar.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/rendering/sliver.dart';
+import 'package:portfolio6/utils/_utils.dart';
 
 class GenSliverAppBar extends StatelessWidget {
-  const GenSliverAppBar({super.key});
+  const GenSliverAppBar({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return SliverAppBar(
-      title: Text('a'),
+    return SliverLayoutBuilder(
+      builder: (_, SliverConstraints constraints) {
+        const double height = kToolbarHeight;
+
+        return SliverAppBar(
+          pinned: true,
+          expandedHeight: height * 1.5,
+          collapsedHeight: height,
+          surfaceTintColor: Colors.transparent,
+          backgroundColor: Color.lerp(
+            Colors.transparent,
+            Colors.black.withAlpha(100),
+            (constraints.scrollOffset / (height * 0.5)).clamp(0, 1),
+          ),
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text('a'),
+          ),
+        );
+      },
     );
   }
 }
