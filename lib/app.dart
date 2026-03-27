2f43a3c1e5a526944963db611b4b98fcb18074e8
Lukasz <intergenixyt@gmail.com>
2026-03-27 11:41:41 +0100

    init

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio6/screens/home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: .fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
+      ),
+      home: const MyHomePage(
+        title: 'Flutter Demo Home Page',
+      ),
+    );
+  }
+}
