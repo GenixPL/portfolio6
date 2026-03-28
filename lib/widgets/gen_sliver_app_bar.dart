7c4e4f3e518deb2538183da883fc58bb26281a30
Lukasz <intergenixyt@gmail.com>
2026-03-28 12:00:49 +0100

    initial app bar

--- lib/widgets/gen_sliver_app_bar.dart
+++ lib/widgets/gen_sliver_app_bar.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:portfolio6/theme/_theme.dart';
 
 class GenSliverAppBar extends StatelessWidget {
   const GenSliverAppBar({
@@ -14,30 +17,81 @@ class GenSliverAppBar extends StatelessWidget {
 
         final Color backgroundColor = Theme.of(context).colorScheme.surface;
 
-        return SliverAppBar(
-          pinned: true,
-          expandedHeight: height * 1.5,
-          collapsedHeight: height,
-          surfaceTintColor: Colors.transparent,
-          backgroundColor: Color.lerp(
-            backgroundColor,
-            Color.lerp(backgroundColor, Colors.black, 0.35),
-            (constraints.scrollOffset / (height * 0.5)).clamp(0, 1),
-          ),
-          flexibleSpace: FlexibleSpaceBar(
-            title: Text('a'),
-          ),
-          bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(3),
-            child: AnimatedContainer(
-              height: 3,
-              duration: Duration(milliseconds: 300),
-              curve: Curves.ease,
-              color: constraints.scrollOffset > height ? Colors.black.withAlpha(200) : Colors.transparent,
+        final double scale = max(height, height * 1.5 - constraints.scrollOffset) / height;
+
+        return PinnedHeaderSliver(
+          child: Container(
+            height: height * scale,
+            color: Color.lerp(
+              backgroundColor,
+              Color.lerp(backgroundColor, Colors.black, 0.35),
+              (constraints.scrollOffset / (height * 0.5)).clamp(0, 1),
+            ),
+            child: _buildBody(
+              context: context,
+              scale: scale,
+              scrollOffset: constraints.scrollOffset,
             ),
           ),
         );
       },
+    );
+  }
+
+  Widget _buildBody({
+    required BuildContext context,
+    required double scale,
+    required double scrollOffset,
+  }) {
+    final TextScaler textScaler = TextScaler.linear(scale);
+
+    const double bottomLineHeight = 3;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        const SizedBox(height: bottomLineHeight),
+
+        Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 12 * scale,
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Text(
+                  '|_|0|_|\n|_|_|0|\n|0|0|0|',
+                  textScaler: textScaler,
+                  style: TextStyle(
+                    fontFamily: FontFamily.cpMono.assetName,
+                    fontSize: 8.0,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 12.0),
+              // TODO(genix): center this
+              Text(
+                // Letter sizes are strange due to the font.
+                'łN',
+                textScaler: textScaler,
+                style: TextStyle(
+                  fontFamily: FontFamily.kontanter.assetName,
+                  fontSize: 20.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        AnimatedContainer(
+          height: bottomLineHeight,
+          duration: Duration(milliseconds: 300),
+          curve: Curves.ease,
+          color: scrollOffset > 0 ? Colors.black.withAlpha(200) : Colors.transparent,
+        ),
+      ],
     );
   }
 }
