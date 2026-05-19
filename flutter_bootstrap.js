99c6f282a0350d455081edca4e6352d7ba390a73
GenixPL <intergenixyt@gmail.com>
2026-05-19 21:51:02 +0000

    Deploying to gh-pages from @ GenixPL/portfolio6@1347ebe8f956750ac93c343496dc8ccc9f39fae0 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1148579884" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "894691328" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
