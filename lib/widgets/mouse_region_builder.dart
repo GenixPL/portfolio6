6febc9395e0b334472f71a0a15a552f40ace6eb6
Lukasz <intergenixyt@gmail.com>
2026-06-02 17:58:15 +0200

    fix hit

--- lib/widgets/mouse_region_builder.dart
+++ lib/widgets/mouse_region_builder.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:portfolio6/widgets/_widgets.dart';
 
 class MouseRegionBuilder extends StatefulWidget {
